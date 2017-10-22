ee741e93e1288d351a4abb6ad9820ff9a83b7c86
Linas <qlp@Linass-MacBook-Pro.local>
2017-10-22 20:44:41 +0300

    Setup apollo server to use express

---
+++ apollo-server/server.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import { graphqlExpress } from 'apollo-server-express';
+
+const PORT = 3000;
+var app = express();
+
+app.use('/graphql', bodyParser.json(), graphqlExpress({ schema: myGraphQLSchema }));
+app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
+
+app.listen(PORT);
